4fafb84fc81cedfc24a44ec43e0240f6a1b40008
Guillaume Blackburn <guillaume.blackburn1@gmail.com>
2021-01-15 21:51:41 +0400

    design correction

--- app/src/main/java/mu/guillaumebl/finalproject/MainActivity.kt
+++ app/src/main/java/mu/guillaumebl/finalproject/MainActivity.kt
@@ -12,18 +12,9 @@ import mu.guillaumebl.finalproject.ui.main.MainViewModel
 
 class MainActivity : AppCompatActivity() {
 
-//    private lateinit var viewModel: MainViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
-//        viewModel.nearStationData.observe(this, {
-//            Log.i(LOG_TAG, it.data.nearstations.distinct().toString())
-//            for (station in it.data.nearstations) {
-//                Log.i(LOG_TAG, station.street_name)
-//            }
-//            Log.i(LOG_TAG, it.data.nearstations.distinct().count().toString())
-//        })
         setContentView(R.layout.activity_main)
         initToolbar()
     }
